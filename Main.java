55b4c1553cceb49f63e737adb7cda39f2fcbcb16
ArnoldC98 <matteoloiacono82@gmail.com>
2023-01-23 17:53:07 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
+
 
 public class Main{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Hello world!");
-        int x = 1;
-        x++;
-
+        
+        
     }
 }
